629ef186733400a1f3586122cb32d458280a2a83
Marina Balashova <159475191+Marina700@users.noreply.github.com>
2025-02-17 20:10:25 +0300

    Update task4.py

--- Task4/task4.py
+++ Task4/task4.py
@@ -1,4 +1,8 @@
-massive = open(input(), 'r', encoding = 'utf-8')
+import sys
+script,massive_file = sys.argv
+print('Введите путь к файлу с данными о элементах массива')
+massive_file=input()
+massive = open(massive_file, 'r', encoding = 'utf-8')
 massive_list = list(map(str.strip, massive.readlines()))
 int_massive = [int(i) for i in massive_list]
 mid = sorted(int_massive)[len(int_massive) // 2]
